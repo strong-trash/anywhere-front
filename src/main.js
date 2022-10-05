04f8401fc19545666c98bf9f723be46fbd14e1b0
wonjinYi <studioplug17@gmail.com>
2022-10-06 03:20:37 +0900

    Set vue-select

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
+
 import router from "./router.js";
 import App from "./App.vue";
+
+// vue-select
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+Vue.component("v-select", vSelect);
 
 Vue.config.productionTip = false;
 
